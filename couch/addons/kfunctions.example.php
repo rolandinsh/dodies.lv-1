87612c96fdf6de404487d5e60c0b6fed1d0ac02f
Normunds Rustanovics <normis@Normundss-MacBook-Pro.local>
2018-02-28 13:57:23 +0200

    new couch

--- couch/addons/kfunctions.example.php
+++ couch/addons/kfunctions.example.php
@@ -1,11 +1,6 @@
 <?php
 if ( !defined('K_COUCH_DIR') ) die(); // cannot be loaded directly
 
-require_once( K_COUCH_DIR.'addons/multi-lang/multi-lang.php' );
-require_once( K_COUCH_DIR.'addons/data-bound-form/data-bound-form.php' );
-require_once( K_COUCH_DIR.'addons/csv/csv.php' );
-require_once( K_COUCH_DIR.'addons/bootstrap-grid/bootstrap-grid.php' );
-
 //require_once( K_COUCH_DIR.'addons/cart/cart.php' );
 //require_once( K_COUCH_DIR.'addons/inline/inline.php' );
 //require_once( K_COUCH_DIR.'addons/extended/extended-folders.php' );
