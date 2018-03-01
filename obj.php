92d6ee999c39d7ef1f672413d66b861b7731d011
Normunds Rustanovics <normis@Normundss-iMac-2.local>
2018-03-01 20:23:50 +0200

    grid

--- obj.php
+++ obj.php
@@ -53,9 +53,11 @@
     <link rel="stylesheet" href="/css/dodies.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   </head>
   <body>
  
@@ -98,11 +100,15 @@
 
       <div class="jumbotron">
         <h1><cms:show k_page_title /></h1>
-
-        <p><cms:get "mazais_<cms:show k_lang />" /></p>
-        <hr />
-        <p><cms:get "apraksts_<cms:show k_lang />" /></p>
-        
+	<div class="container-fluid">
+	<div class=".col-12 .col-sm-6 .col-lg-8">        
+		<p><cms:get "mazais_<cms:show k_lang />" /></p>
+        	<p><cms:get "apraksts_<cms:show k_lang />" /></p>
+        </div>
+	<div class=".col-6 .col-lg-4">
+		Stuff
+	</div>
+	</div>
       </div>
 
       <cms:reverse_related_pages 'photo_product' limit='1' orderby='weight'  masterpage='gallery.php' >
