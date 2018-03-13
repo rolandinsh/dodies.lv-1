c5309491a92a8416d45d7201f90dc12be98bfc34
Normunds <normis@dodies.lv>
2018-03-13 07:45:05 +0200

    logo text

--- obj.php
+++ obj.php
@@ -77,7 +77,7 @@
     <!-- navigation menu -->
     <nav class="navbar doc-nav navbar-expand-lg navbar-inverse bg-primary">
         <div class="container no-padding">
-            <a class="navbar-brand" href="/"><img src="/icons/green-logo.png" class="d-inline-block align-center" style="width: 30px; height: 30px; border-radius: 3px" alt="dodies.lv"/></a>
+            <a class="navbar-brand" href="/"><img src="/icons/green-logo.png" class="d-inline-block align-center" style="width: 30px; height: 30px; border-radius: 3px" alt="dodies.lv"/>&nbsp; dodies.lv</a>
 
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
